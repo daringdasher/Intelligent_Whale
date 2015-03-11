8bea973a1ce31dd094ae43b43ee6dcd87b65347b
unknown <dieselmanm@gmail.com>
2015-03-11 15:41:47 -0400

    Created  and commented the parent classes.

---
+++ Breach_Of_Contract/Breach_Of_Contract/Map.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Breach_Of_Contract
+{
+    //Class that creates, and handels the main map (and possibly the mini-map)
+    class Map
+    {
+    }
+}
---
+++ Breach_Of_Contract/Breach_Of_Contract/Object.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Breach_Of_Contract
+{
+    //Parent Class to Wall and Cover
+    class Object
+    {
+    }
+}
