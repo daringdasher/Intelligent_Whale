645295ee2c5012fa2d90e5338b93a9cbb9a176c6
Tim Yuan <ytlee239@yahoo.com>
2015-04-03 23:13:59 -0400

    - Added a Bullet Class for the time being - One enemy now spawns in - Player 1 now fires a bullet with spacebar

---
+++ Breach_Of_Contract/Breach_Of_Contract/Bullet.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace Breach_Of_Contract
+{
+    class Bullet:Object
+    {
+        //Attributes
+        protected Vector2 position;
+        protected Rectangle bulletRect;
+        protected float rotation;
+
+        //Properties
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public float Rotation
+        {
+            get { return rotation; }
+        }
+        //Constructor
+        public Bullet()
+        {
+            position = new Vector2(0, 0);
+            bulletRect = new Rectangle(0, 0, 64, 64);
+
+        }
+        
+        public void move(Vector2 dest)
+        {
+            if (dest != Vector2.Zero)
+            {
+                Vector2 vector = new Vector2(dest.X - position.X, dest.Y - position.Y);
+                Vector2 unitVector = Vector2.Normalize(vector);
+                Vector2 newPosition = new Vector2(position.X + unitVector.X * 2, position.Y + unitVector.Y * 2);
+                position = newPosition;
+                bulletRect = new Rectangle((int)position.X - 16, (int)position.Y - 16, 64,64);
+                rotation = (float)Math.Atan2(vector.X, -vector.Y);
+            }
+
+        }
+    }
+}
--- Breach_Of_Contract/Breach_Of_Contract/Game1.cs
+++ Breach_Of_Contract/Breach_Of_Contract/Game1.cs
@@ -24,6 +24,10 @@ namespace Breach_Of_Contract
         Texture2D brownSprite;
         Texture2D orangeSprite;
         Texture2D scottSprite;
+        Texture2D enemy1Sprite;
+        Texture2D enemy2Sprite;
+        Texture2D enemy3Sprite;
+        Texture2D bulletSprite;
         Player player1;
         Player player2;
         Player player3;
@@ -32,15 +36,20 @@ namespace Breach_Of_Contract
         Vector2 p2Destination;
         Vector2 p3Destination;
         Vector2 p4Destination;
+        Vector2 bulletDest;
         String currentPlayer;
         Enemy testEnemy;
         float player1Rot = 0;
         float player2Rot = 0;
         float player3Rot = 0;
         float player4Rot = 0;
+        float bulletRot = 0;
         bool startScreenActive;
         Map map;
         List<object> objectList = new List<object>();
+        Random rgen;
+        Bullet bullet;
+        bool bulletActive = false;
         
         //Constructor
         public Game1()
@@ -89,6 +98,15 @@ namespace Breach_Of_Contract
             player2 = (Player)objectList[1]; brownSprite = Content.Load<Texture2D>("Players/Brown");
             player3 = (Player)objectList[2]; orangeSprite= Content.Load<Texture2D>("Players/Orange");
             player4 = (Player)objectList[3]; scottSprite = Content.Load<Texture2D>("Players/Scott");
+            enemy1Sprite = Content.Load<Texture2D>("Enemies/Enemy1");
+            enemy2Sprite = Content.Load<Texture2D>("Enemies/Enemy2");
+            enemy3Sprite = Content.Load<Texture2D>("Enemies/Enemy3");
+            bulletSprite = Content.Load<Texture2D>("Bullet");
+            bullet = new Bullet();
+            rgen = new Random();
+            float enemyX = rgen.Next(0, 721);
+            float enemyY = rgen.Next(0, 1281);
+            testEnemy = new Enemy(new Vector2(enemyX, enemyY));
             foreach (object element in objectList)
             {
             }
@@ -117,14 +135,14 @@ namespace Breach_Of_Contract
 
             // TODO: Add your update logic here
             mousePosition = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
-            if (Mouse.GetState().LeftButton == ButtonState.Pressed) 
+            if (Mouse.GetState().LeftButton == ButtonState.Pressed) // move selected character with LMB
             {
                 if (player1.ID == currentPlayer) { p1Destination = mousePosition; }
                 if (player2.ID == currentPlayer) { p2Destination = mousePosition; }
                 if (player3.ID == currentPlayer) { p3Destination = mousePosition; }
                 if (player4.ID == currentPlayer) { p4Destination = mousePosition; }
             }
-            if (Mouse.GetState().RightButton == ButtonState.Pressed)
+            if (Mouse.GetState().RightButton == ButtonState.Pressed) // select character with RMB
             {
                 mousePosition = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
                 if (player1.PlayerRect.Contains(mousePosition)) {currentPlayer = player1.ID;}
@@ -132,11 +150,21 @@ namespace Breach_Of_Contract
                 if (player3.PlayerRect.Contains(mousePosition)) {currentPlayer = player3.ID;}
                 if (player4.PlayerRect.Contains(mousePosition)) {currentPlayer = player4.ID;}
             }
+            
+            if(Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                bulletActive = true;
+                if (player1.ID == currentPlayer) { bullet.Position = player1.Position; bulletDest = testEnemy.Position; }
+                if (player2.ID == currentPlayer) { bullet.Position = player2.Position; bulletDest = testEnemy.Position; }
+                if (player3.ID == currentPlayer) { bullet.Position = player3.Position; bulletDest = testEnemy.Position; }
+                if (player4.ID == currentPlayer) { bullet.Position = player1.Position; bulletDest = testEnemy.Position; }
+            }
+            
             player1.move(p1Destination); player1Rot = player1.Rotation;
             player2.move(p2Destination); player2Rot = player2.Rotation;
             player3.move(p3Destination); player3Rot = player3.Rotation;
             player4.move(p4Destination); player4Rot = player4.Rotation;
-
+            if (bulletActive) bullet.move(bulletDest); bulletRot = bullet.Rotation;
             if ((player1.Position.X > p1Destination.X - 1) && (player1.Position.X < p1Destination.X + 1) && (player1.Position.Y > p1Destination.Y - 1) && (player1.Position.Y < p1Destination.Y + 1)) p1Destination = new Vector2(0, 0);
             if ((player2.Position.X > p2Destination.X - 1) && (player2.Position.X < p2Destination.X + 1) && (player2.Position.Y > p2Destination.Y - 1) && (player2.Position.Y < p2Destination.Y + 1)) p2Destination = new Vector2(0, 0);
             if ((player3.Position.X > p3Destination.X - 1) && (player3.Position.X < p3Destination.X + 1) && (player3.Position.Y > p3Destination.Y - 1) && (player3.Position.Y < p3Destination.Y + 1)) p3Destination = new Vector2(0, 0);
@@ -159,7 +187,10 @@ namespace Breach_Of_Contract
             spriteBatch.Draw(brownSprite, player2.Position, new Rectangle(0, 0, 256, 256), Color.White, player2Rot, new Vector2(128, 128), .25f, SpriteEffects.None, 0F);
             spriteBatch.Draw(orangeSprite, player3.Position, new Rectangle(0, 0, 256, 256), Color.White, player3Rot, new Vector2(128, 128), .25f, SpriteEffects.None, 0F);
             spriteBatch.Draw(scottSprite, player4.Position, new Rectangle(0, 0, 256, 256), Color.White, player4Rot, new Vector2(128, 128), .25f, SpriteEffects.None, 0F);
+            
 
+            spriteBatch.Draw(enemy1Sprite, testEnemy.Position,new Rectangle(0, 0, 256, 256), Color.White, bulletRot - 180, new Vector2(128, 128), .25f, SpriteEffects.None, 0F);
+            spriteBatch.Draw(bulletSprite, bullet.Position, new Rectangle(0, 0, 64, 64), Color.White, bulletRot, new Vector2(32, 32), .5f, SpriteEffects.None, 0F);
             spriteBatch.End();
             base.Draw(gameTime);
         }
